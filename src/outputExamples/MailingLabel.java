cefbfdfd43cff88cc68f7416b5351da154256cca
Tania <22mishrt@elmbrookstudents.org>
2018-09-13 13:52:24 -0500

    Mailing address (escape sequences)

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,9 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		System.out.println("Tania Mishra\n1400 Independence Dr.\nBrookfield, WI 53045");
+	}
+
+}
