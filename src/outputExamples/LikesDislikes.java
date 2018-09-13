e7a50821c1eff9805c1a7f3b98fb5e3d769430fa
Tania <22mishrt@elmbrookstudents.org>
2018-09-13 13:49:42 -0500

    Likes and Dislikes (in columns)

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		System.out.println("Likes\t\t\tDislikes");
+		System.out.println("1. Reading\t\t1. Non-fiction books");
+		System.out.println("2. Dancing\t\t2. Yard work");
+		System.out.println("3. Eating\t\t3. Cleaning my room");
+		System.out.println("4. Strawberries\t\t4. No organization");
+		System.out.println("5. Blue\t\t\t5. Cooking");
+
+	}
+
+}
