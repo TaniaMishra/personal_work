ae85753a1f82dc43ff47570197689af23bd119c6
Tania <22mishrt@elmbrookstudents.org>
2018-09-13 14:01:18 -0500

    Initials program

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,17 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTTTTTTTTT  RRRRRR    MM       MM");
+		System.out.println("        T          R     R   M M     M M");
+		System.out.println("        T          R     R   M  M   M  M");
+		System.out.println("        T          RRRRRR    M   M M   M");
+		System.out.println("        T          R  R      M    M    M");
+		System.out.println("        T          R   R     M         M");
+		System.out.println("        T          R    R    M         M");
+		System.out.println("        T          R     R   M         M");
+
+	}
+
+}
