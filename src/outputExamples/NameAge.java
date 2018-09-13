5eef27351851594306af51ea03c5967ef9761045
Tania <22mishrt@elmbrookstudents.org>
2018-09-13 13:17:27 -0500

    Name and Age

---
+++ src/outputExamples/NameAge.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class NameAge {
+
+	public static void main(String[] args) {
+		System.out.println("Tania");
+		System.out.println("Mishra");
+		System.out.println("= " + 14);
+	}
+
+}
