922d5d5779e8fa2472fa9b65aa1d3b25699ec543
Tania <22mishrt@elmbrookstudents.org>
2018-09-18 18:52:16 -0500

    Write Code from Directions Part 2

---
+++ src/outputExamples/WriteCode2.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class WriteCode2 {
+
+	public static void main(String[] args) {
+		final double MYSTERYNUMBER = 13.33;
+		int dead = 1234567;
+		final char MIDDLEINITIAL = 'Z';
+		System.out.println("Constants: " + MYSTERYNUMBER + ", " + MIDDLEINITIAL);
+	}
+
+}
