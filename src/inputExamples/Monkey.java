b53eb80ca897fdbe2e9fcdaba4f01d3a595f910f
Tania <22mishrt@elmbrookstudents.org>
2018-09-20 12:58:15 -0500

    Updated Monkey program

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Monkey {
 
 	public static void main(String[] args) {
-		double height;
-		Scanner input = new Scanner(System.in);
+		double height = -999.0;		//made it an impossible number so that it is easier to debug.
+		Scanner userInput = new Scanner(System.in);
 		System.out.println("What is the height of an adult monkey? Just type the number (in feet).");
-		height = input.nextDouble();
+		height = userInput.nextDouble();
 		System.out.println("The height of an adult monkey is " + height + " feet.");
 	}
 
