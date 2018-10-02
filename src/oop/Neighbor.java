53a4f379e160141bb402c128a7b37362f84157b8
Tania <22mishrt@elmbrookstudents.org>
2018-10-02 14:05:08 -0500

    Neighbor class and it's test class

---
+++ src/oop/Neighbor.java
@@ -0,0 +1,26 @@
+package oop;
+
+public class Neighbor {
+
+	public Neighbor() {
+		
+	}
+	
+	public void sayJoke() {
+		System.out.println("Knock Knock");
+		System.out.println("Who's there?");
+		System.out.println("Interupting cow");
+		System.out.println("Interupting c...");
+		System.out.println("Moooo!");
+
+	}
+	public void sayMoney() {
+		System.out.println("I want my money.");
+	}
+	public void sayConversation() {
+		System.out.println("Hello friend! How are you?");
+		System.out.println("I'm good, thanks!");
+		System.out.println("I gotta go. Bye!");
+		System.out.println("Bye!");
+	}
+}
