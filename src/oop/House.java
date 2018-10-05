1f9c638000d8c3883ab01083cc423a27ca873246
Tania <22mishrt@elmbrookstudents.org>
2018-10-05 15:05:56 -0500

    Updated House & Test Class

--- src/oop/House.java
+++ src/oop/House.java
@@ -21,5 +21,24 @@ public class House {
 		
 		System.out.println("It will take " + time + " minutes to paint the wall.");
 	}
+	public void plant(double rows) {
+		Scanner input = new Scanner(System.in);
+		final double WIDTH = 10.0;
+		System.out.println("What is the length of the garden? (ft.)");
+		double length = input.nextDouble();
+
+		double timeLinearFt = length/7.5;    //linear feet per hour (15 ft/2 hr = 7.5 ft/2hr)
+		double totalTime = timeLinearFt * rows;
+
+		System.out.println("It will take " + totalTime + " hours to plant the garden.");
+	}
+	public void pool(double radius, double height) {
+		double gallons = Math.PI * radius * radius * height;
+		double hours = gallons/300;
+
+		System.out.println("It will take " + hours + " hours to fill up the pool.");
+	
+	}
+
 	
 }
