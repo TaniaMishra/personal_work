7919047ab6a9c51624d48ed4981bf45d450e433f
Tania <22mishrt@elmbrookstudents.org>
2018-10-05 15:06:08 -0500

    Added Methods to Numbers

--- src/oop/Numbers.java
+++ src/oop/Numbers.java
@@ -11,11 +11,25 @@ public class Numbers {
 	}
 	public void sayNumberPlus2(double number) {
 		number += 2;
-		System.out.println("Number + 2 = " + number);
+		System.out.println("Your number + 2 = " + number);
 	}
 	public void saySum(double value1, double value2) {
 		double sum = value1 + value2;
 		System.out.println(value1 + " + " + value2 + " = " + sum);
+	}
+	public double returnSquare(double number) {
+		double value = number * number;
+		return value;
+	}
+	public double returnArea(double length, double width) {
+		double area = length * width;
+		return area;
+	}
+	public int returnRoundUp(double number) {
+		number ++;
+		int value = (int)number;
+		return value;
+		//or use: int value = Math.ceil(number); and return value;
 	}
 	
 }
--- src/oop/TestNumbers.java
+++ src/oop/TestNumbers.java
@@ -6,8 +6,22 @@ public class TestNumbers {
 	public static void main(String[] args) {
 		//Scanner input = new Scanner(System.in);
 		Numbers bob = new Numbers();
-		bob.sayNumber(7.5);
-		bob.sayNumberPlus2(5.0);
-		bob.saySum(2.1, 1.4);
+		
+		bob.sayNumber(5.5);
+		
+		bob.sayNumberPlus2(5.5);
+		
+		bob.saySum(2.1, 3.4);
+		
+		double value = bob.returnSquare(3.0);
+		System.out.println("3.0 squared = " + value);
+		
+		double area = bob.returnArea(12.0, 12.0);
+		System.out.println("Area: " + area);
+		
+		double number = bob.returnRoundUp(4.5);
+		System.out.println("Round Up: " + number);
+		
+		
 	}
 }
