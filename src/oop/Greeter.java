4c728d75d775bb1afa5a39f4111cd189329079e5
Tania <22mishrt@elmbrookstudents.org>
2018-10-09 15:03:03 -0500

    Added to Greeter (instance fields)

--- src/oop/Greeter.java
+++ src/oop/Greeter.java
@@ -4,16 +4,25 @@ package oop;
 
 public class Greeter {
 	//instance fields - data storage
+	private int age;
+	
 	//constructors - methods for constructing the object
-	
-	
 	public Greeter() {
 		// Statements to initialize the state of the object
+		age = 14;
 	}
 	
 	
 	//methods - things the object can do
 	public void sayHello() {
 		System.out.println("Hello!");
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int newAge) {
+		age = newAge;
+		System.out.println("Age: " + age);
+
 	}
 }
---
+++ src/oop/TestGreeter.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class TestGreeter {
+
+	public static void main(String[] args) {
+		Greeter bob = new Greeter();
+		System.out.println("Age: " + bob.getAge());
+		bob.setAge(15);
+
+	}
+
+}
