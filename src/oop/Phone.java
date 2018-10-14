d79df6425b7eba1f1595ab4a0fd1d6d43f27d92c
Tania <22mishrt@elmbrookstudents.org>
2018-10-14 12:47:39 -0500

    Finished Phone Class and it's test class

--- src/oop/Phone.java
+++ src/oop/Phone.java
@@ -4,7 +4,7 @@ public class Phone {
 	private long phoneNum;
 	private long speedDial;
 	private long lastCall;
-	private int emergency;
+	private final int EMERGENCY;
 	private long numCalls;
 	private double numMin;
 	
@@ -12,7 +12,7 @@ public class Phone {
 		phoneNum = 1111111111;
 		speedDial = 1111111111;
 		lastCall = 1111111111;
-		emergency = 911;
+		EMERGENCY = 911;
 		numCalls = 999;
 		numMin = 9999;
 	}
@@ -20,10 +20,51 @@ public class Phone {
 		phoneNum = newPhoneNum;
 		speedDial = newSpeedDial;
 		lastCall = newLastCall;
-		emergency = 911;
+		EMERGENCY = 911;
 		numCalls = newNumCalls;
 		numMin = newNumMin;
 	}
 	
+	public long getNum() {
+		return phoneNum;
+	}
+	public long getSpeedDial() {
+		return speedDial;
+	}
+	public long getLastCall() {
+		return lastCall;
+	}
+	public int getEmergency() {
+		return EMERGENCY;
+	}
+	public long getNumCalls() {
+		return numCalls;
+	}
+	public double getNumMin() {
+		return numMin;
+	}
+	
+	public void setNum(long diffNum) {
+		phoneNum = diffNum;
+	}
+	public void setSpeedDial(long diffSpeedDial) {
+		speedDial = diffSpeedDial;
+	}
+	public void setLastCall(long diffLastCall) {
+		lastCall = diffLastCall;
+	}
+	public void setNumCalls(long diffNumCalls) {
+		numCalls = diffNumCalls;
+	}
+	public void setNumMin(double diffNumMin) {
+		numMin = diffNumMin;
+	}
+	
+	
+	public void makeCall(long numCalled, double min) {
+		lastCall = numCalled;
+		numMin += min;
+		numCalls++;
+	}
 	
 }
