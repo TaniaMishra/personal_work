4d896fcc571cda3efacbb83b859fb3608589e623
Tania M <22mishrt@elmbrookstudents.org>
2018-10-17 14:43:47 -0500

    Largest Number: if, else if, else

---
+++ src/decisions/Largest.java
@@ -0,0 +1,27 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class Largest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int one = input.nextInt();
+		System.out.println("Enter another number: ");
+		int two = input.nextInt();
+		System.out.println("Enter another number: ");
+		int three = input.nextInt();
+		
+		if (one > two && one > three) {
+			System.out.println("The largest integer is: " + one);
+		}
+		else if (two > one && two > three) {
+			System.out.println("The largest integer is: " + two);
+		}
+		else {
+			System.out.println("The largest integer is: " + three);
+		}
+	}
+
+}
