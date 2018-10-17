d82aa62c9e97fd22a8ecf2ff105d7cafe08dbd91
Tania <22mishrt@elmbrookstudents.org>
2018-10-17 17:14:05 -0500

    Started Circle Class

---
+++ src/decisions/Circle.java
@@ -0,0 +1,11 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class Circle {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		input.close();
+	}
+}
