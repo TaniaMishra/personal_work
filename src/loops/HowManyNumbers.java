48d1119fc18738c1c1a8954ee8179ad3c1111984
Tania M <22mishrt@elmbrookstudents.org>
2018-10-26 10:40:41 -0500

    Started How Many Numbers Program

---
+++ src/loops/HowManyNumbers.java
@@ -0,0 +1,19 @@
+package loops;
+
+import java.util.Scanner;
+
+public class HowManyNumbers {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = input.nextInt();
+		int sum = 0;
+		do {
+			sum += number;
+			System.out.println("Sum: " + sum);
+			
+		}
+		while ()
+
+	}
+}
