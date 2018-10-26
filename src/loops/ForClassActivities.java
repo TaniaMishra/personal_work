1aeaaa3ef901aa2bf10d15dcb50650358ac84527
Tania M <22mishrt@elmbrookstudents.org>
2018-10-26 10:54:45 -0500

    For loops class activities day 2

--- src/loops/ForClassActivities.java
+++ src/loops/ForClassActivities.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class ForClassActivities {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		//Example 1
-		for(int counter = 1; counter <= 5; counter++) {
+		//Example
+/*		for(int counter = 1; counter <= 5; counter++) {
 			System.out.println(counter);
 		}
 		
@@ -40,11 +40,31 @@ public class ForClassActivities {
 			System.out.println("HI!");
 		}
 
+		//#5
 		System.out.println("What is your favorite number?");
 		int favNum = input.nextInt();
 		for(int i = 0; i < 10; i++) {
 			System.out.println(favNum + " Hello!");
 		}
+
+		//#6
+		int sum = 0;
+		for (int i = 1; i <= 10; i ++) {
+			sum += i;
+		}
+		System.out.println("Sum of digits from 0 - 10: " + sum);
+*/
+		//#9
+		int number = 0;
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter a number:");
+			number = input.nextInt();
+			sum += number;
+		}
+		System.out.println("The sum of the numbers is: " + sum);
+		
+		
 		input.close();
 	}
 }
