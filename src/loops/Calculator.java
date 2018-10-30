51fd67ad622f7434cf01254f1e2ded1b2518f105
Tania M <22mishrt@elmbrookstudents.org>
2018-10-30 15:08:47 -0500

    Started Calculator

---
+++ src/loops/Calculator.java
@@ -0,0 +1,25 @@
+package loops;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Calculator");
+		System.out.println("1. Is it a right triangle?");
+		System.out.println("2. Powers");
+		System.out.println("3. Square Root");
+		System.out.println("4. Round Up");
+		System.out.println("5. Round Down");
+		System.out.println("6. Quit");
+		int choice = input.nextInt();
+		
+		switch (choice) {
+		
+		}
+		
+		
+
+	}
+}
