b12defd74f9b7d560d5dd99852dfe20d6b35ae12
Tania M <22mishrt@elmbrookstudents.org>
2018-10-31 15:07:16 -0500

    Tried prime factorization

--- src/loops/PrimeFactorization.java
+++ src/loops/PrimeFactorization.java
@@ -9,25 +9,47 @@ public class PrimeFactorization {
 		int number = input.nextInt();
 		int primeCounter = 0;
 		int counter = 0;
+		int tester;
 		boolean prime;
-		int primeNum;
-		
-		for (int i = 2; i < number; i++) {
-			int tester = number % 1;
-			if (tester != 0) {
-				primeCounter++;
+		//int primeNum;
+		int j = 2;	//j is a number less than NUMBER
+		while (j <= number) {
+			//part one: check if j is prime
+			for (int i = 2; i <= j; i++) {	//i is used to see if j is prime
+				tester = j % i;
+				//System.out.println("j % i = " + tester);
+				if (tester != 0) {
+					primeCounter++;
+				}
+				counter++;
+			}
+			if (primeCounter == counter) {
+				prime = true;	//j is prime
+			}
+			else {
+				prime = false;	//j is not prime
+			}
+			//part two: check if j is a factor of NUMBER (only if it is prime)
+			if (prime == true) {
+				tester = number % j;
+				if (tester == 0) {
+					System.out.println(j);
+					number = number/j;
+					j = 1;	//reset everything because the loop will now do the same thing but with a new NUMBER (new NUMBER = old NUMBER / j)
+				}
+				else {
+					j++;	//if j isn't a factor of number, do it again and check the next value of j
+				}
+			}
+			else {
+				j++;	//if j isn't prime, do it again and check with the next value of j
 			}
 		}
-		if (primeCounter == counter) {
-			prime = true;
-		}
-		else {
-			prime = false;
-		}
+		
 		
 		
 		//checks if a number is prime
-		for (int i = 2; i < number; i++) {
+		/*for (int i = 2; i < number; i++) {
 			int tester = number % 1;
 			if (tester != 0) {
 				primeCounter++;
@@ -41,7 +63,7 @@ public class PrimeFactorization {
 			prime = false;
 		}
 		System.out.println("Prime? " + prime);
-
-		
+*/
+		input.close();
 	}
 }
