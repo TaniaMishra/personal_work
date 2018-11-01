74c3e55e65f60ce2abd6746028dcce6afbc26c06
Tania M <22mishrt@elmbrookstudents.org>
2018-11-01 14:10:43 -0500

    Cleaned up Prime Factorization Program

--- src/loops/PrimeFactorization.java
+++ src/loops/PrimeFactorization.java
@@ -43,28 +43,6 @@ public class PrimeFactorization {
 				}
 			}
 		}
-		/*
-		 * int primeCounter = 0; int counter = 0; int tester; boolean prime;
-		 * //int primeNum; int j = 2; //j is a number less than NUMBER while (j
-		 * <= number) { //part one: check if j is prime for (int i = 2; i <= j;
-		 * i++) { //i is used to see if j is prime tester = j % i;
-		 * System.out.println("j = " + j); System.out.println("i = " + i);
-		 * System.out.println("j % i = " + tester); if (tester != 0) {
-		 * primeCounter++; System.out.println("prime counter = " +
-		 * primeCounter); } counter++; System.out.println("counter: " +
-		 * counter); } if (primeCounter == counter) { prime = true; //j is
-		 * prime\ System.out.println("Prime = " + prime); } else { prime =
-		 * false; //j is not prime } //part two: check if j is a factor of
-		 * NUMBER (only if it is prime) if (prime == true) { tester = number %
-		 * j; System.out.println("Went into loop for prime = true"); if (tester
-		 * == 0) { System.out.println(j); number = number/j; j = 1; //reset
-		 * everything because the loop will now do the same thing but with a new
-		 * NUMBER (new NUMBER = old NUMBER / j) } else { j++; //if j isn't a
-		 * factor of number, do it again and check the next value of j } } else
-		 * { j++; //if j isn't prime, do it again and check with the next value
-		 * of j } counter = 0; }
-		 */
-
 		input.close();
 	}
 }
