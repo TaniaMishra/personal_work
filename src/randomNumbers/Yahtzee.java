76385e66858767e70736e3737b10426ed6b73a85
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-11-07 14:19:00 -0600

    Yahtzee Version 1

---
+++ src/randomNumbers/Yahtzee.java
@@ -0,0 +1,34 @@
+package randomNumbers;
+
+import java.util.Random;
+
+public class Yahtzee {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int max = 6;
+		int min = 1;
+		int roll = 0;
+		int count = 0;
+		int tries = 0;
+		boolean yahtzee = false;
+
+		while (yahtzee = false) {
+			for (int i = 0; i < 5; i++) {
+				int outcome = min + rand.nextInt(max - min + 1);
+				if (roll == 0) {
+					roll = outcome;
+					count++;
+				} else if (outcome == roll) {
+					count++;
+				}
+			}
+			if (count == 5) {
+				System.out.println("You got Yahtzee! It took you " + tries + " tries!");
+				yahtzee = true;
+			} else {
+				yahtzee = false;
+				tries++;
+			}
+		}
+	}
+}
