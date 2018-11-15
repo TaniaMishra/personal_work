66fc23aebda1e93f17040b9e0e7834f22565ad74
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-11-15 14:12:43 -0600

    Worked on Number of Letters

--- src/strings/NumberOfLetters.java
+++ src/strings/NumberOfLetters.java
@@ -10,14 +10,31 @@ public class NumberOfLetters {
 		int vowels = 0;
 		int consonants = 0;
 		int punctuation = 0;
+		int numbers = 0;
 		int length = sentence.length();
 		
 		for (int i = 0; i < length; i++) {
 			char character = sentence.charAt(i);
-			if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
-				
+			if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y') {
+				vowels++;
+			}
+			else if (character > 65 && character <= 90) {
+				consonants++;
+			}
+			else if (character > 97 && character <= 122) {
+				consonants++;
+			}
+			else if (character >= 48 && character <= 57){
+				numbers++;
+			}
+			else {
+				punctuation++;
 			}
 		}
+		
+		System.out.println("Number of vowels: " + vowels);
+		System.out.println("Number of consonants: " + consonants);
+		System.out.println("Number of punctuation marks: " + punctuation);
 
 	}
 }
