8d19cb59449742ba4f7ff749db5f57d715ede37f
Tania <22mishrt@elmbrookstudents.org>
2018-11-27 19:27:33 -0600

    Started SSN Breakdown
    How do you get an integer out of a String?

---
+++ src/arrays/SSNBreakdown.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SSNBreakdown {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a social serurity number in the format XXX-XX-XXXX:");
+		String ssn = input.next();
+		int dash = ssn.indexOf('-');
+		String ending = ssn.substring(dash+1);
+		dash = ending.indexOf('-');
+		String middle = ending.substring(0, dash);
+		
+
+	}
+}
