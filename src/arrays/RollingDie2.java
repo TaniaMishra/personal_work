30b37c896ab25902d8b83e33ae3dc72a5d7e4cca
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-12-03 15:06:14 -0600

    Rolling A Die - Versions 1, 2, and 3

---
+++ src/arrays/RollingDie2.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RollingDie2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.println("How many students are in the class?");
+		int students = input.nextInt();
+		System.out.println("How many sides are on the die?");
+		int max = input.nextInt();
+		int min = 1;
+		System.out.println("How many times should each student roll the die?");
+		int rollsEach = input.nextInt();
+		
+		int[][] rolls = new int[students][rollsEach];
+		
+		for (int r = 0; r < students; r++) {
+			for (int c = 0; c < rollsEach; c++) {
+				rolls[r][c] = min + rand.nextInt(max-min+1);
+			}
+		}
+
+		for (int r = 0; r < students; r++) {
+			for (int c = 0; c < rollsEach; c++) {
+				System.out.print(rolls[r][c] + "  ");
+			}
+			System.out.println("");
+		}
+		
+		for (int c = 0; c < rollsEach; c++) {
+			for (int r = 0; r < students; r++) {
+				System.out.print(rolls[r][c] + "  ");
+			}
+			System.out.println("");
+		}
+		input.close();
+	}
+}
