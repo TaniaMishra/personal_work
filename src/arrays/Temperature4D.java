e03e27e35ea6138776390b4cfa22373ac04ec313
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-12-04 15:08:55 -0600

    Started the Temperature 4D Program

---
+++ src/arrays/Temperature4D.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import java.util.Random;
+
+public class Temperature4D {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int min = -20;
+		int max = 110;
+		int[][][][] temp = new int[10][12][4][7];
+		
+
+	}
+}
