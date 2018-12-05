77008763a44b3ea6a00860712b6b716c86f5eaa0
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-12-05 15:10:38 -0600

    Continued Temperature 4D

--- src/arrays/Temperature4D.java
+++ src/arrays/Temperature4D.java
@@ -8,7 +8,39 @@ public class Temperature4D {
 		int min = -20;
 		int max = 110;
 		int[][][][] temp = new int[10][12][4][7];
+		for (int y = 0; y < temp.length; y++) {
+			for (int m = 0; m < temp[0].length; m++) {
+				for (int w = 0; w < temp[0][0].length; w++) {
+					for (int d = 0; d < temp[0][0][0].length; d++) {
+						temp[y][m][w][d] = min + rand.nextInt(max-min+1);
+					}
+				}
+			}
+		}
 		
+		int greatestTemp = min;
+		int greatestY = 0;
+		int greatestM = 0;
+		int greatestW = 0;
+		int greatestD = 0;
+
+		for (int y = 0; y < temp.length; y++) {
+			for (int m = 0; m < temp[0].length; m++) {
+				for (int w = 0; w < temp[0][0].length; w++) {
+					for (int d = 0; d < temp[0][0][0].length; d++) {
+						if (temp[y][m][w][d] > greatestTemp) {
+							greatestTemp = temp[y][m][w][d];
+							greatestY = y;
+							greatestM = m;
+							greatestW = w;
+							greatestD = d;
+						}
+					}
+				}
+			}
+		}
+		
+		System.out.println("The greatest daily temperature was " + greatestTemp + " degrees on " + greatestM + "/" + greatestD + "/" + greatestY + " (week " + greatestW + ").");
 
 	}
 }
