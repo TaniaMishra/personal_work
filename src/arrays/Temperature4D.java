981e2e82a6374f9d9d266c6f10870fc65696b8a9
Tania <22mishrt@elmbrookstudents.org>
2018-12-05 21:26:58 -0600

    Finished Temperature 4D

--- src/arrays/Temperature4D.java
+++ src/arrays/Temperature4D.java
@@ -42,5 +42,45 @@ public class Temperature4D {
 		
 		System.out.println("The greatest daily temperature was " + greatestTemp + " degrees on " + greatestM + "/" + greatestD + "/" + greatestY + " (week " + greatestW + ").");
 
+
+		double daysSum = 0;
+		double counter = 0;
+		double monthAverage = 0;
+		double highestMonthTemp = -20;
+		double highestY = -1;
+		double highestM = -1;
+		
+		for (int y = 0; y < temp.length; y++) {
+			for (int m = 0; m < temp[0].length; m++) {
+				for (int w = 0; w < temp[0][0].length; w++) {
+					for (int d = 0; d < temp[0][0][0].length; d++) {
+						daysSum += temp[y][m][w][d];
+						counter++;
+					}
+				}
+				monthAverage = daysSum/counter;
+				if (monthAverage > highestMonthTemp) {
+					highestMonthTemp = monthAverage;
+					highestY = y;
+					highestM = m;
+				}
+				daysSum = 0;
+			}
+		}
+		
+		System.out.println("The highest average monthly temperature was " + highestMonthTemp + " degrees in month " + highestM + ", year " + highestY + ".");
+		
+		for (int m = 0; m < temp[0].length; m++) {
+			for (int w = 0; w < temp[0][0].length; w++) {
+				for (int d = 0; d < temp[0][0][0].length; d++) {
+					System.out.print(temp[0][m][w][d] + "   ");
+				}
+				System.out.println("");
+			}
+			System.out.println("");
+		}
+		
+		
+		
 	}
 }
