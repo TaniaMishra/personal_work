25c329144177dba86ed4f28fe9dc663b1981f21f
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-12-07 15:07:03 -0600

    Student and it's Test Class

---
+++ src/arrays/Student.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Student {
+	private String name;
+	private int score;
+	
+	public Student(String xName, int xScore) {
+		name = xName;
+		score = xScore;
+	}
+	
+	public int returnScore() {
+		return score;
+	}
+	
+	public String returnName() {
+		return name;
+	}
+}
