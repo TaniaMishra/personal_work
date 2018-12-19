cd89af1632b874667d863bce97d07fafb408999b
Tania Mishra <22mishrt@elmbrookstudents.org>
2018-12-19 15:08:41 -0600

    Started Books Sequential

--- src/searching/BookSequential.java
+++ src/searching/BookSequential.java
@@ -1,10 +1,24 @@
 package searching;
 
-public class BookSequential {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class BookSequential {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		ArrayList<Book> books = new ArrayList<Book>();
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Enter the title:");
+			String title = input.nextLine();
+			System.out.println("Enter the number of pages:");
+			int pages = input.nextInt();
+			books.add(new Book(title, pages));
+		}
+		
+		for (Book x : books) {
+			x.getPages();
+		}
 
 	}
-
 }
