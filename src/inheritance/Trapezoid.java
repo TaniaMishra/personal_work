0d1dd0810fb7196a41e12fa0b4d30149c9e7a8d7
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-01-11 15:05:51 -0600

    Trapezoid

---
+++ src/inheritance/Trapezoid.java
@@ -0,0 +1,33 @@
+package inheritance;
+
+public class Trapezoid extends Quadrilateral implements Geometry {
+	private double height;
+	
+	public Trapezoid() {
+		super(5, 6, 5, 10);
+		double leg = Math.abs(super.getSide2()-super.getSide4());
+		double temp = (super.getSide1()*super.getSide1()) - (leg*leg);
+		height = Math.sqrt(temp);
+	}
+	public Trapezoid(double one, double two, double three, double four) {
+		super(one, two, three, four);
+		double leg = Math.abs(super.getSide2()-super.getSide4());
+		double temp = (super.getSide1()*super.getSide1()) - (leg*leg);
+		height = Math.sqrt(temp);
+	}
+
+	public double getHeight() {
+		return height;
+	}
+	
+	public void setHeight(double h) {
+		height = h;
+	}
+	
+	public double area() {
+		double a = (super.getSide1() + super.getSide3())/2;
+		a = a * height;
+		return a;
+	}
+
+}
