3c9e7814fbeeee4c1cfc988a259a9a61d0969341
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-01-11 15:06:03 -0600

    Rectangle (Modified)

--- src/inheritance/Rectangle.java
+++ src/inheritance/Rectangle.java
@@ -1,21 +1,10 @@
 package inheritance;
 
-public class Rectangle extends Quadrilateral {
+public class Rectangle extends Parallelogram implements Geometry {
 	public Rectangle() {
-		this.setSide1(10);
-		this.setSide2(10);
-		this.setSide3(10);
-		this.setSide4(10);
+		super(10, 10, 10);
 	}
 	public Rectangle(double l, double w) {
-		this.setSide1(l);
-		this.setSide2(w);
-		this.setSide3(l);
-		this.setSide4(w);
-	}
-	
-	public double area() {
-		double a = this.getSide1() * this.getSide2();
-		return a;
+		super(l, w, l);
 	}
 }
