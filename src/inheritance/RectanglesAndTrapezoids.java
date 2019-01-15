99f30e501f2123384fe22e367b21ebbfefc8a7d3
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-01-15 14:57:10 -0600

    Added Random Numbers to Rectangles and Trapezoids Test Class

--- src/inheritance/RectanglesAndTrapezoids.java
+++ src/inheritance/RectanglesAndTrapezoids.java
@@ -1,11 +1,27 @@
 package inheritance;
 
+import java.util.Random;
+
 public class RectanglesAndTrapezoids {
 	public static void main(String[] args) {
-		Rectangle rec1 = new Rectangle();
-		Trapezoid trap1 = new Trapezoid();
-		Rectangle rec2 = new Rectangle();
-		Trapezoid trap2 = new Trapezoid();
+		Random rand = new Random();
+		
+		int length = 1 + rand.nextInt(10);
+		int width = 1 + rand.nextInt(10);
+		Rectangle rec1 = new Rectangle(length, width);
+		length = 1 + rand.nextInt(10);
+		width = 1 + rand.nextInt(10);
+		Rectangle rec2 = new Rectangle(length, width);
+		int s1 = 1 + rand.nextInt(10);
+		int s2 = 1 + rand.nextInt(10);
+		int s3 = 1 + rand.nextInt(10);
+		int s4 = 1 + rand.nextInt(10);
+		Trapezoid trap1 = new Trapezoid(s1, s2, s3, s4);
+		s1 = 1 + rand.nextInt(10);
+		s2 = 1 + rand.nextInt(10);
+		s3 = 1 + rand.nextInt(10);
+		s4 = 1 + rand.nextInt(10);
+		Trapezoid trap2 = new Trapezoid(s1, s2, s3, s4);
 		
 		System.out.println(rec1.toString());
 		System.out.println(rec2.toString());
